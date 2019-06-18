b44991233864758e76a22f8c5414c87842e8cb83
Your Name <you@example.com>
2019-06-18 19:36:37 +0800

    add jk.sh

---
+++ jk.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "CPU负载:$(uptime  | awk '{print $8,$9,$10}')"
+echo "网卡流量:$(ifconfig eth0 | awk '/RX p/{print $3}')"
+echo "内存剩余容量:$(free -m | awk '/M/{print $4}')"
+echo -e "磁盘剩余容量:\n$(df -h | awk '{print $4}')"
+echo "计算机账户数量:$(cat /etc/passwd | awk -F: 'BEGIN{x=0}{$1;x++}END{print x}')"
+echo "当前登录账户数量:$(who | wc -l)"
+echo "计算机当前开启的进程数:$(ps aux | awk 'BEGIN{a=0}{$1 a++}END{print a}')"
+echo "本机已安装的软件包数量:$(rpm -qa | wc -l)"
+
+
